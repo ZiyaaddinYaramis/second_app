19e39239c71c6dabf4b6e941e051a7c26155b822
Ziyaaddin Yaramis <ziyaaddinyaramis@gmail.com>
2024-10-15 10:20:38 +0300

    15.10.24

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+// Main fonksiyonu, uygulamanın başlangıç noktasıdır.
 void main() {
   runApp(const Uygulamam());
 }
 
-// Görunum tasarımı
+// Görunum tasarımı, StatelessWidget sınıfından türetilmiştir.
 class Uygulamam extends StatelessWidget {
   const Uygulamam({super.key});
 
+
+
+
+// build metodu, görsel tasarımın oluşturulduğu yerdir.
+
+//Android ve iOS için farklı tasarımlar oluşturabilir.
+//MaterialApp ve CupertinoApp widgetleri ile uygulama tasarımı belirlenir.
+//Anroid için Material tasarımı, iOS için Cupertino tasarımı kullanilir.
+// MaterialApp, uygulamanın genel temasını belirler.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -18,3 +28,20 @@ class Uygulamam extends StatelessWidget {
     );
   }
 }
+
+// Scaffold widgeti, uygulamanın temel yapısını oluşturur.
+// Scaffold; AppBar, Drawer, BottomNavigationBar, FloatingActionButton, SnackBar, BottomSheet, ve daha birçok bileşeni içerir.
+
+
+
+
+
+
+
+
+
+
+// AppBar: AppBar widgeti, uygulamanın üst kısmında yer alır.
+// AppBar, başlık, arka plan rengi, menü, arama alanı gibi özellikler içerir.
+// AppBar, ust kısmında yer alır ve genellikle uygulamanın başlığını, menüsünü, arama alanını içerir.
+
