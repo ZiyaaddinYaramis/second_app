237d11dfba1d2823405b1060be5489dd48af15cb
Ziyaaddin Yaramis <ziyaaddinyaramis@gmail.com>
2024-10-21 16:43:33 +0300

    21.10.24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 
 // Main fonksiyonu, uygulamanın başlangıç noktasıdır.
 void main() {
@@ -20,11 +20,17 @@ class Uygulamam extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            const Text('Merhaba Dünya!'),
-            Image.asset('assets/images/logo.png', width: 200, height: 200),
-          ],
+        appBar: AppBar(
+          title: const Text('Second App - AppBar'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('Merhaba Dünya!'),
+              Image.asset('assets/images/logo.png', width: 200, height: 200),
+              Text('It\'s time to learn Flutter!'),
+            ],
+          ),
         ),
       ),
     );
@@ -33,6 +39,12 @@ class Uygulamam extends StatelessWidget {
 
 // Scaffold widgeti, uygulamanın temel yapısını oluşturur.
 // Scaffold; AppBar, Drawer, BottomNavigationBar, FloatingActionButton, SnackBar, BottomSheet, ve daha birçok bileşeni içerir.
+
+
+// Center widgeti, çocuk widgetini merkeze alır.
+// Column widgeti, çocuk widgetlerini dikey olarak sıralar.
+// Column widgeti, children parametresi ile birden fazla widget alabilir.
+
 
 
 
